59cb5e99f76f58fedb60136d41f3805eb8c75133
Markus Bulheller <markus.bulheller@gmail.com>
2022-12-30 13:14:47 +0100

    add tracks and cars to navbar

--- src/components/UI/navbar.tsx
+++ src/components/UI/navbar.tsx
@@ -1,29 +1,31 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
-    const menuItems = [
-        { name: 'Home', href: '/', active: true },
-        { name: 'Events', href: '/events', active: false },
-        { name: 'Drivers', href: '/drivers', active: false },
-    ]
+  const menuItems = [
+    { name: "Home", href: "/", active: true },
+    { name: "Events", href: "/events", active: false },
+    { name: "Drivers", href: "/drivers", active: false },
+    { name: "Tracks", href: "/tracks", active: false },
+    { name: "Cars", href: "/cars", active: false },
+  ];
 
-    return (
-        <nav className="flex h-24 items-center justify-center bg-gray-900 align-text-bottom text-xl text-white shadow-md shadow-red-600 sm:justify-between sm:px-24">
-            <div className="hidden sm:block">
-                <Image src="/w2w.png" alt="Logo" width={100} height={50} />
-            </div>
-            <ul className=" flex h-full items-center  justify-center gap-8 md:justify-end">
-                {menuItems.map((item) => {
-                    return (
-                        <li key={item.name} className="hover:text-red-600">
-                            <Link href={item.href}>{item.name}</Link>
-                        </li>
-                    )
-                })}
-            </ul>
-        </nav>
-    )
-}
+  return (
+    <nav className="flex h-24 items-center justify-center bg-gray-900 align-text-bottom text-xl text-white shadow-md shadow-red-600 sm:justify-between sm:px-24">
+      <div className="hidden sm:block">
+        <Image src="/w2w.png" alt="Logo" width={100} height={50} />
+      </div>
+      <ul className=" flex h-full items-center  justify-center gap-8 md:justify-end">
+        {menuItems.map((item) => {
+          return (
+            <li key={item.name} className="hover:text-red-600">
+              <Link href={item.href}>{item.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
