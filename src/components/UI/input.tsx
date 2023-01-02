d5059de946f5b653562cf04283f6f0c50acfd726
Markus Bulheller <markus.bulheller@gmail.com>
2023-01-02 22:16:46 +0100

    add select input, error handling

--- src/components/UI/input.tsx
+++ src/components/UI/input.tsx
@@ -1,16 +1,19 @@
 import TimezoneSelect from "react-timezone-select";
 import React from "react";
+import { string } from "zod";
 
 type InputProps = {
   name: string;
   type?: string;
   value?: {};
   onChange?: any;
+  options?: { name: string; value: string | number }[];
+  error?: string;
 };
 
 // eslint-disable-next-line react/display-name
 const Input = React.forwardRef((props: InputProps, ref: any) => {
-  const { name, type = "text", value = {}, onChange } = props;
+  const { name, type = "text", value = {}, onChange, options, error } = props;
   if (type === "checkbox") {
     return (
       <div className="mt-4">
@@ -40,6 +43,34 @@ const Input = React.forwardRef((props: InputProps, ref: any) => {
         />
       </div>
     );
+  } else if (type === "select" || type === "duration") {
+    return (
+      <div className="flex flex-col pt-4">
+        <label htmlFor={name}>
+          {name.charAt(0).toUpperCase() + name.slice(1)}
+        </label>
+        <select
+          id={name}
+          // @ts-ignore
+          value={value}
+          onChange={(e) =>
+            onChange(
+              type === "duration" ? parseInt(e.target.value) : e.target.value
+            )
+          }
+          className="rounded border-2 border-transparent bg-gray-200 p-2 text-gray-900 caret-red-600 hover:border-red-600 focus:border-red-600 focus:outline-0 md:w-72"
+        >
+          {options &&
+            options.map((option) => {
+              return <option value={option.value}>{option.name}</option>;
+            })}
+          <option value="" disabled selected hidden>
+            Please Choose...
+          </option>
+        </select>
+        {error && error}
+      </div>
+    );
   }
   return (
     <div className="flex flex-col pt-4">
@@ -53,6 +84,7 @@ const Input = React.forwardRef((props: InputProps, ref: any) => {
         step="any"
         className="rounded border-2 border-transparent bg-gray-200 p-2 text-gray-900 caret-red-600 hover:border-red-600 focus:border-red-600 focus:outline-0 md:w-72"
       />
+      {error && error}
     </div>
   );
 });
