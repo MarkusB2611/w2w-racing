e2a5b9690f37cc5c4e632a8a34387e06ecc9cd04
Markus Bulheller <markus.bulheller@gmail.com>
2023-01-02 23:43:55 +0100

    event router add to app

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -2,13 +2,15 @@ import { router } from "../trpc";
 import { authRouter } from "./auth";
 import { carRouter } from "./car";
 import { driverRouter } from "./driver";
+import { eventRouter } from "./event";
 import { trackRouter } from "./track";
 
 export const appRouter = router({
   driver: driverRouter,
   auth: authRouter,
   track: trackRouter,
-  car: carRouter
+  car: carRouter,
+  event: eventRouter
 });
 
 // export type definition of API
